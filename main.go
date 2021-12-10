a6ba358780214320663c3d023a9396cdee7510b7
Mike Christofilopoulos <mhristof@gmail.com>
2021-12-10 16:32:38 +0000

    initial import

--- main.go
+++ main.go
@@ -1,1 +1,29 @@
+package main
 
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	file, err := os.Open("input1")
+	if err != nil {
+		panic(err)
+	}
+
+	one(bufio.NewReader(file))
+}
+
+func one(in *bufio.Reader) int {
+	for {
+		line, err := in.ReadString('\n')
+		if err != nil {
+			return 0
+		}
+
+		fmt.Println(fmt.Sprintf("line: %+v", line))
+	}
+
+	return 0
+}
---
+++ main_test.go
@@ -0,0 +1,19 @@
+package main
+
+// func TestOne(t *testing.T) {
+// 	cases := []struct {
+// 		name string
+// 		in   *bufio.Reader
+// 		out  int
+// 	}{
+// 		{
+// 			name: "",
+// 			in:   bufio.NewReader(strings.NewReader("some string")),
+// 			out:  7,
+// 		},
+// 	}
+
+// 	for _, test := range cases {
+// 		assert.Equal(t, test.out, one(test.in), test.name)
+// 	}
+// }
